f2cf90ba4195203d0e851a223a55a318ab6a04b4
amartuvsh1n <48076179+amartuvsh1n@users.noreply.github.com>
2021-12-22 15:44:16 +0800

    Update migration.sh
    add virt-v2v

--- migration.sh
+++ migration.sh
@@ -1,47 +1,62 @@
 source .env
 source /etc/os-release
+. $adminrc
 
 dnf-centos() 
 {
-    #add repo
-    dnf install -y epel-release && dnf update;
-    #install packages
-    dnf install -y qemu-utils jq;
+    echo debian
+    # #add repo
+    # dnf install -y epel-release && dnf update;
+    # #install packages
+    # dnf install -y qemu-utils jq python3-openstackclient;
 }
 
 apt-ubuntu()
 {
-    apt update;
-    apt install -y qemu-utils jq;
+    echo ubuntu
+    #apt update;
+    #apt install -y qemu-utils jq python3-openstackclient;
 }
 
 
 migration()
 {
+    # echo start date: $date |& tee /var/log/out.log
     # read all dsName
     jq -r '.[].dsName' vm.json | while read dsName; do
+        size=$(jq -r ".[] | select( .dsName == \"$dsName\" ).vms[].disk_size" vm.json)
+        ip=$(jq -r ".[] | select( .dsName == \"$dsName\" ).vms[].ip" vm.json)
+        flavor=$(jq -r ".[] | select( .dsName == \"$dsName\" ).vms[].flavor" vm.json)
         # read vmname from dsName
-        jq -r ".[] | select( .dsName == \"$dsName\" ).vms[].name" vm.json | while read vmname; do
+        jq -r ".[] | select( .dsName == \"$dsName\" ).vms[].name" vm.json | while read vmname; 
+        do
             for sub_name in -ctk '' -flat
             do
                 # if space in vmname, swap to %20 
                 name=${vmname// /%20}
-                curl  -k -u $Username:$Password "$base_url/$name/$name$sub_name.vmdk?dcPath=$dcPath&dsName=$dsName" > "$vmname$sub_name.vmdk"
-
-                # convert vmdk to qcow2
-                if [ -f "$vmname.vmdk" ] 
-                then
-                    # convert to qcow2
-                    qemu-img convert -f vmdk -O qcow2 "$vmname.vmdk" "$vmname.qcow2"
-                    # create vm from qcow2
-                    virt-install --name=linuxconfig-vm \
-                    --vcpus=1 \
-                    --memory=1024 \
-                    --disk path=$(pwd)/"$vmname.qcow2",bus=virtio \
-                    --graphics none 
-                fi
-
+                dcPath=${dcPath//-/%252d}
+                echo curl  -k -u $Username:$Password "$base_url/$name/$name$sub_name.vmdk?dcPath=$dcPath&dsName=$dsName" --output "$vmname$sub_name.vmdk"
+                curl  -k -u $Username:$Password "$base_url/$name/$name$sub_name.vmdk?dcPath=$dcPath&dsName=$dsName" --output "$vmname$sub_name.vmdk"
             done
+
+            # convert vmdk to qcow2
+            if [ -f "$vmname-flat.vmdk" ] 
+            then
+                echo qemu-img convert -f vmdk -O qcow2 "$vmname.vmdk" "$vmname.qcow2"
+                qemu-img convert -f vmdk -O qcow2 "$vmname$sub_name.vmdk" "$vmname$sub_name.qcow2"
+                echo intall virt-io
+                virt-v2v -i disk "$vmname.qcow2" -o local -os "./"
+                echo openstack image create --disk-format qcow2 --container-format bare --file "$vmname-sda" --public "$vmname"
+                openstack image create --disk-format qcow2 --container-format bare --file "$vmname-sda" --public "$vmname"
+                echo openstack volume create --bootable  --image "$vmname" --size $size "$vmname volume" 
+                openstack volume create --bootable  --image "$vmname" --size $size "$vmname volume" 
+                portid=$(openstack port create --network $network --fixed-ip subnet=$subnet,ip-address="$ip" --format value -c id "ip for $vmanem")
+                #echo portID=$portid
+                echo openstack server create --flavor "$flavor" --volume "$vmname volume" --nic port-id=$portid "$vmname"
+                openstack server create --flavor "$flavor" --volume "$vmname volume" --nic port-id=$portid "$vmname"
+                #echo "done date: $date" |$ tee /var/log/out.log
+            fi
+
         done
     done
 }
